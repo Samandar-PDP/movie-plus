4befdd82cbecca465c1001192e450b03ce1e86c7
samandar-me <samandarcoderdev@gmail.com>
2024-01-09 16:01:21 +0500

    F

--- lib/screen/detail_screen.dart
+++ lib/screen/detail_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:movie_plus/model/movie_response.dart';
 
@@ -11,6 +12,28 @@ class DetailScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(movie.title ?? ""),
+      ),
+      body: ListView(
+        children: [
+          Image.network("https://image.tmdb.org/t/p/w500${movie.backdropPath}"),
+          const SizedBox(height: 20),
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Text(movie.voteCount.toString(),style: TextStyle(fontSize: 20),),
+                    const Icon(CupertinoIcons.star,color: Colors.orange,),
+                    Expanded(child: Align(alignment: AlignmentDirectional.centerEnd,child: Icon(CupertinoIcons.bookmark),))
+                  ],
+                ),
+                const SizedBox(height: 5,),
+                Text(movie.overview.toString())
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
